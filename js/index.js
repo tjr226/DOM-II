ed22e160151d44a234fbb4212bc3da98148555bf
tjr226 <tjr226@gmail.com>
2019-04-09 15:34:44 -0400

    cleaned up commented out code

--- js/index.js
+++ js/index.js
@@ -93,16 +93,3 @@ mountainExcursionDiv.addEventListener('click', function() {
     mountainExcursionDiv.style.backgroundColor="gray";
 });
 
-
-// let featuresH4 = document.querySelector('.main-content .top-content:nth-child(1) h4');
-// featuresH4.textContent = (siteContent["main-content"]["features-h4"]);
-
-
-// let clickMeButton = document.querySelector('.container .button');
-// clickMeButton.addEventListener('click', function () { clickMeButton.style.backgroundColor = "green" });
-// clickMeButton.addEventListener('click', (event) => { event.stopPropagation() });
-
-// clickMeButton.addEventListener('mouseenter', function () {
-//     clickMeButton.style.backgroundColor = 'grey';
-//     clickMeButton.style.border = '1px solid black';
-//     clickMeButton.style.color = 'white';
